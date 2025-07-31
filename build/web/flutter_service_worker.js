59326a01acd5eb25fcf6c485d18dc406d15e057c
KongNhel <kabkong94@gmail.com>
2025-07-31 17:58:23 +0700

    Deploy Flutter web app to GitHub Pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -56,7 +56,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "85fc17692fc1aa980e1ff2ac9a1d1c65
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "1cb45dc5f06c14a2ee825b8e187f130e",
+"flutter_bootstrap.js": "67c95cbe678c4c93f8b2bf11692f8122",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
